38cd426bc61460ce08e8e5ad56b264b44667aa00
Rishi Shah <bobbungee@gmail.com>
2011-06-02 16:19:26 -0500

    Cleaned up flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
+      @user.update_attributes(:password => params[:user][:password])
       @title = "Sign up"
       render 'new'
     end
